129b5bbdfc73313b060831d5808ff6d8d7bd6fb4
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-03-06 07:37:03 +0100

    Change folder structure

--- UserManagementAPI/UserManagementAPI/src/main/kotlin/com/blazc/usermanagementapi/dao/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.blazc.usermanagementapi.dao
-
-import com.blazc.usermanagementapi.vao.User
-import org.bson.types.ObjectId
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface UserRepository : MongoRepository<User, ObjectId> {
-
-}
