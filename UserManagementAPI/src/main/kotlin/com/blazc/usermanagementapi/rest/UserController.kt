24108f2af8e61c29ff9de1ce462f18a9d0de8535
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-03-06 17:48:10 +0100

    Add logger

--- UserManagementAPI/src/main/kotlin/com/blazc/usermanagementapi/rest/UserController.kt
+++ UserManagementAPI/src/main/kotlin/com/blazc/usermanagementapi/rest/UserController.kt
@@ -6,10 +6,16 @@ import org.bson.types.ObjectId
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.util.logging.Logger
+
 
 @RestController
 @CrossOrigin
 class UserController(private val dao: UserRepository) {
+
+    companion object {
+        private val log = Logger.getLogger(UserController::class.java.toString())
+    }
 
     @GetMapping("/users")
     fun getAllUsers(): Iterable<com.blazc.usermanagementapi.dto.User> {
@@ -20,6 +26,7 @@ class UserController(private val dao: UserRepository) {
     fun getUserById(@PathVariable id: String): ResponseEntity<com.blazc.usermanagementapi.dto.User> {
         val user = dao.findById(ObjectId(id))
         if (user.isEmpty) {
+            log.info { "/users/$id ; User not found!" }
             return ResponseEntity(HttpStatus.NOT_FOUND)
         }
         return ResponseEntity.ok(user.get().toDto())
@@ -35,6 +42,7 @@ class UserController(private val dao: UserRepository) {
     fun updateUser(@PathVariable id: String, @RequestBody user: com.blazc.usermanagementapi.dto.User): ResponseEntity<com.blazc.usermanagementapi.dto.User> {
         val existingUser = dao.findById(ObjectId(id))
         if (existingUser.isEmpty) {
+            log.info {"/users/$id ; User not found!" }
             return ResponseEntity(HttpStatus.NOT_FOUND)
         }
 
@@ -49,6 +57,7 @@ class UserController(private val dao: UserRepository) {
     fun deleteUser(@PathVariable id: String): ResponseEntity<String> {
         val existingUser = dao.findById(ObjectId(id))
         if (existingUser.isEmpty) {
+            log.info {"/users/$id ; User not found!" }
             return ResponseEntity(HttpStatus.NOT_FOUND)
         }
 
