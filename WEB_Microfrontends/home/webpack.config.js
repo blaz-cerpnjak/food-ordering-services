f315dac97b23fd92106c611cbb5c72944bd73c4d
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-04-17 16:42:02 +0200

    Dockerize microfrontends

--- WEB_Microfrontends/home/webpack.config.js
+++ WEB_Microfrontends/home/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (_, argv) => ({
     allowedHosts: "all",
     proxy: {
       "/api/v1": {
-        target: "http://localhost:8080/api/v1",
+        target: "http://web-api-gateway:8080/api/v1",
         changeOrigin: true,
         secure: false,
         pathRewrite: { "^/api/v1": "" },
